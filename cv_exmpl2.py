1467af62ef3d4cc7e12a43b9a56cf94ed4d702f9
moti041 <94522651+moti041@users.noreply.github.com>
2022-02-03 10:33:26 +0200

    Add files via upload

---
+++ cv_exmpl2.py
@@ -0,0 +1,79 @@
+# https://docs.opencv.org/4.x/dd/d3b/tutorial_py_svm_opencv.html
+from __future__ import print_function
+from __future__ import division
+import sys
+sys.path.append('C:/Users\Admin\Downloads\opencv-master')
+import cv2 as cv
+import numpy as np
+import argparse
+import cv2 as cv
+print(cv.__file__)
+import numpy as np
+import argparse
+
+SZ=20
+bin_n = 16 # Number of bins
+def deskew(img):
+    affine_flags = cv.WARP_INVERSE_MAP|cv.INTER_LINEAR
+    SZ=20
+    m = cv.moments(img)
+    if abs(m['mu02']) < 1e-2:
+        return img.copy()
+    skew = m['mu11']/m['mu02']
+    M = np.float32([[1, skew, -0.5*SZ*skew], [0, 1, 0]])
+    img = cv.warpAffine(img,M,(SZ, SZ),flags=affine_flags)
+    return img
+def hog(img):
+    bin_n = 16 # Number of bins
+    gx = cv.Sobel(img, cv.CV_32F, 1, 0)
+    gy = cv.Sobel(img, cv.CV_32F, 0, 1)
+    mag, ang = cv.cartToPolar(gx, gy)
+    bins = np.int32(bin_n*ang/(2*np.pi))    # quantizing binvalues in (0...16)
+    bin_cells = bins[:10,:10], bins[10:,:10], bins[:10,10:], bins[10:,10:]
+    mag_cells = mag[:10,:10], mag[10:,:10], mag[:10,10:], mag[10:,10:]
+    hists = [np.bincount(b.ravel(), m.ravel(), bin_n) for b, m in zip(bin_cells, mag_cells)]
+    hist = np.hstack(hists)     # hist is a 64 bit vector
+    return hist
+img = cv.imread(r"C:\Users\Admin\Downloads\opencv-master\data\digits.png",0)
+#cv.imshow('image',img)
+if img is None:
+    raise Exception("we need the digits.png image from samples/data here !")
+#hist = hog(img)
+cells = [np.hsplit(row,100) for row in np.vsplit(img,50)]
+# First half is trainData, remaining is testData
+train_cells = [ i[:50] for i in cells ]
+test_cells = [ i[50:] for i in cells]
+deskewed = [list(map(deskew,row)) for row in train_cells]
+hogdata = [list(map(hog,row)) for row in deskewed]
+trainData = np.float32(hogdata).reshape(-1,64)
+responses = np.repeat(np.arange(10),250)[:,np.newaxis]
+if src is None:
+    print('Could not open or find the image:', args.input)
+    exit(0)
+bgr_planes = cv.split(src)
+histSize = 256
+histRange = (0, 256) # the upper boundary is exclusive
+accumulate = False
+b_hist = cv.calcHist(bgr_planes, [0], None, [histSize], histRange, accumulate=accumulate)
+g_hist = cv.calcHist(bgr_planes, [1], None, [histSize], histRange, accumulate=accumulate)
+r_hist = cv.calcHist(bgr_planes, [2], None, [histSize], histRange, accumulate=accumulate)
+hist_w = 512
+hist_h = 400
+bin_w = int(round( hist_w/histSize ))
+histImage = np.zeros((hist_h, hist_w, 3), dtype=np.uint8)
+cv.normalize(b_hist, b_hist, alpha=0, beta=hist_h, norm_type=cv.NORM_MINMAX)
+cv.normalize(g_hist, g_hist, alpha=0, beta=hist_h, norm_type=cv.NORM_MINMAX)
+cv.normalize(r_hist, r_hist, alpha=0, beta=hist_h, norm_type=cv.NORM_MINMAX)
+for i in range(1, histSize):
+    cv.line(histImage, ( bin_w*(i-1), hist_h - int(b_hist[i-1]) ),
+            ( bin_w*(i), hist_h - int(b_hist[i]) ),
+            ( 255, 0, 0), thickness=2)
+    cv.line(histImage, ( bin_w*(i-1), hist_h - int(g_hist[i-1]) ),
+            ( bin_w*(i), hist_h - int(g_hist[i]) ),
+            ( 0, 255, 0), thickness=2)
+    cv.line(histImage, ( bin_w*(i-1), hist_h - int(r_hist[i-1]) ),
+            ( bin_w*(i), hist_h - int(r_hist[i]) ),
+            ( 0, 0, 255), thickness=2)
+cv.imshow('Source image', src)
+cv.imshow('calcHist Demo', histImage)
+cv.waitKey()
